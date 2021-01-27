dccc077754c1c8a4fc6867a3ffb160ec60360806
codeblue25 <chois1031@gmail.com>
2021-01-27 19:31:28 +0900

    210127 퍼즐 버튼 기능, 결과값 구현하기
    뒤집기, 좌우로 밀기 버튼에 따라 문자열 버튼도 움직이게 구현
    첫 줄에 있는 문자와 문자열 버튼의 일치여부에 따른 결과값 화면에 출력

--- puzzle.js
+++ puzzle.js
@@ -1,44 +1,76 @@
-const textObj = {
-    text : "HELLO"
-};
-document.write(textObj.text + "<br/>");
-const texts=textObj.text.split("") 
-console.log(texts);
-//HTML에 문자열 표시, 문자열로 배열 만들기
+const text = document.querySelector(".word").innerHTML;
+const texts = text.split("");
+// console.log(texts);
+let wordBtn = document.querySelector(".wordBtn");
+const btnArr = [];
+const result = document.querySelector(".result");
 
-for(var i = 0; i < texts.length; i++){
+for(var i = 0; i < text.length; i++){
     const btn = document.createElement("button");
-    btn.innerHTML = texts[i];
-    document.body.appendChild(btn);
+    btn.innerHTML = text[i];
+    wordBtn.appendChild(btn);
+    btnArr.push(btn);
 }
-document.write("<br />");
 //문자열 버튼 만들기
 
+const btns = document.querySelector(".btns")
 const reverseBtn = document.createElement("button");
 reverseBtn.innerHTML = "뒤집기"
-document.body.appendChild(reverseBtn);
-const pushBtn = document.createElement("button");
-pushBtn.innerHTML = "밀어내기"
-document.body.appendChild(pushBtn);
+btns.appendChild(reverseBtn);
+const leftBtn = document.createElement("button");
+leftBtn.innerHTML = "왼쪽으로 밀어내기"
+btns.appendChild(leftBtn);
+const rightBtn = document.createElement("button");
+rightBtn.innerHTML = "오른쪽으로 밀어내기"
+btns.appendChild(rightBtn);
 //뒤집기, 밀어내기 버튼 만들기
-
-const result = document.createElement("h4");
-result.innerHTML = "일치합니다."
-document.body.appendChild(result);
-//일치합니다 문자열 표시
 
 const reversing = function(event){
     const reverTexts = texts.reverse();
-    console.log(reverTexts);
+    const reverseStr = reverTexts.join("");
+    for(var i = 0; i < reverseStr.length; i++){
+        btnArr[i].innerHTML = reverseStr[i];
+    }
+    if(text === reverseStr){
+        result.innerHTML = "<strong>일치합니다.</strong>";
+    }else{
+        result.innerHTML = "일치하지 않습니다.";
+    }
 }
-reverseBtn.addEventListener("click", reversing);
-//뒤집기 버튼에 핸들러 함수 작성
+//뒤집기 버튼 함수 + 결과값 함수
 
-const pushing = function(event){
-    let first = texts[0];
-    const shifted = texts.shift();
-    const pushedTexts = texts.push(first);
-    console.log(texts);
+const leftPush = function(event){
+    const leftShift = texts.shift();
+    texts.push(leftShift);
+    const lshiftStr = texts.join("");
+    for(var i = 0; i < lshiftStr.length; i++){
+        btnArr[i].innerHTML = lshiftStr[i];
+    }
+    if(text === lshiftStr){
+        result.innerHTML = "<strong>일치합니다.</strong>";
+    }else{
+        result.innerHTML = "일치하지 않습니다.";
+    }
 }
-pushBtn.addEventListener("click", pushing);
-//밀어내기 버튼에 핸들러 함수 작성
+//왼쪽으로 밀기 버튼 함수 + 결과값 함수
+
+const rightPush = function(event){
+    const rightShift = texts.pop();
+    texts.unshift(rightShift);
+    const rshiftStr = texts.join("");
+    for(var i = 0; i < rshiftStr.length; i++){
+        btnArr[i].innerHTML = rshiftStr[i];
+    }
+    if(text === rshiftStr){
+        result.innerHTML = "<strong>일치합니다.</strong>";
+    }else{
+        result.innerHTML = "일치하지 않습니다.";
+    }
+}
+
+//오른쪽으로 밀기 버튼 함수 + 결과값 함수
+
+reverseBtn.addEventListener("click", reversing);
+leftBtn.addEventListener("click", leftPush);
+rightBtn.addEventListener("click", rightPush);
+//버튼 핸들러 함수 실행
