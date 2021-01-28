b77389ae11dbbb2cbea051ecee2f624d3ab3f4b6
codeblue25 <chois1031@gmail.com>
2021-01-28 16:51:37 +0900

    210128 랜덤 퍼즐 구현하기
    단어 배열에서 랜덤하게 퍼즐 단어 고르기, 퍼즐 단어와 같지만 무작위로 섞인 문자열 버튼 구현하기
    Math.random()이용

--- puzzle.js
+++ puzzle.js
@@ -1,13 +1,22 @@
-const text = document.querySelector(".word").innerHTML;
-const texts = text.split("");
-// console.log(texts);
 let wordBtn = document.querySelector(".wordBtn");
-const btnArr = [];
+const text = document.querySelector(".word");
 const result = document.querySelector(".result");
+const btnArr = [];
+const textArr = ["HELLO", "JAVASCRIPT", "BIBIMNOODLE", "CHILL", "PRIVATE", "OMEGATHREE", "KEYBOARD", "FEBUARY", "STRAWBERRY", "CODEBLUE"];
+const randomStr = function(){
+    const index = Math.floor(Math.random() * textArr.length);
+    return textArr[index];
+}
+const texts = randomStr();
+text.innerHTML = texts;
+const stexts = texts.split("");
+console.log(texts);
+console.log(stexts);
+//첫 줄에 문자열을 문자열 배열에서 랜덤으로 불러오기
 
-for(var i = 0; i < text.length; i++){
+for(var i = 0; i < texts.length; i++){
     const btn = document.createElement("button");
-    btn.innerHTML = text[i];
+    btn.innerHTML = texts[i];
     wordBtn.appendChild(btn);
     btnArr.push(btn);
 }
@@ -26,51 +35,59 @@ btns.appendChild(rightBtn);
 //뒤집기, 밀어내기 버튼 만들기
 
 const reversing = function(event){
-    const reverTexts = texts.reverse();
-    const reverseStr = reverTexts.join("");
+    const reverseStr = stexts.reverse().join("");
     for(var i = 0; i < reverseStr.length; i++){
         btnArr[i].innerHTML = reverseStr[i];
     }
-    if(text === reverseStr){
-        result.innerHTML = "<strong>일치합니다.</strong>";
+    if(texts === reverseStr){
+        result.innerHTML = "<h2>일치합니다.✅</h2>";
     }else{
-        result.innerHTML = "일치하지 않습니다.";
+        result.innerHTML = "😪일치하지 않습니다.😪";
     }
 }
 //뒤집기 버튼 함수 + 결과값 함수
 
 const leftPush = function(event){
-    const leftShift = texts.shift();
-    texts.push(leftShift);
-    const lshiftStr = texts.join("");
+    const leftShift = stexts.shift();
+    stexts.push(leftShift);
+    const lshiftStr = stexts.join("");
     for(var i = 0; i < lshiftStr.length; i++){
         btnArr[i].innerHTML = lshiftStr[i];
     }
-    if(text === lshiftStr){
-        result.innerHTML = "<strong>일치합니다.</strong>";
+    if(texts === lshiftStr){
+        result.innerHTML = "<h2>일치합니다.✅</h2>";
     }else{
-        result.innerHTML = "일치하지 않습니다.";
+        result.innerHTML = "😪일치하지 않습니다.😪";
     }
 }
 //왼쪽으로 밀기 버튼 함수 + 결과값 함수
 
 const rightPush = function(event){
-    const rightShift = texts.pop();
-    texts.unshift(rightShift);
-    const rshiftStr = texts.join("");
+    const rightShift = stexts.pop();
+    stexts.unshift(rightShift);
+    const rshiftStr = stexts.join("");
     for(var i = 0; i < rshiftStr.length; i++){
         btnArr[i].innerHTML = rshiftStr[i];
     }
-    if(text === rshiftStr){
-        result.innerHTML = "<strong>일치합니다.</strong>";
+    if(texts === rshiftStr){
+        result.innerHTML = "<h2>일치합니다.✅</h2>";
     }else{
-        result.innerHTML = "일치하지 않습니다.";
+        result.innerHTML = "😪일치하지 않습니다.😪";
     }
 }
-
 //오른쪽으로 밀기 버튼 함수 + 결과값 함수
 
 reverseBtn.addEventListener("click", reversing);
 leftBtn.addEventListener("click", leftPush);
 rightBtn.addEventListener("click", rightPush);
 //버튼 핸들러 함수 실행
+
+const toggle1 = Math.floor(Math.random() * 2) === 0;
+if(toggle1){
+    reversing();
+}
+const toggle2 = Math.floor(Math.random() * texts.length);
+for(let i = 0; i < toggle2; i++){
+    leftPush();
+}
+//문자열 버튼 랜덤하게 출력하기
